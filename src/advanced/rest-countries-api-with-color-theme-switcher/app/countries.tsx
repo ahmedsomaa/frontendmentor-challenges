44d4cfbe6049093bf4bef3ea8ac96a322098b7f5
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-20 01:14:27 +0200

    refactor: make grid 4 cols

--- src/advanced/rest-countries-api-with-color-theme-switcher/app/countries.tsx
+++ src/advanced/rest-countries-api-with-color-theme-switcher/app/countries.tsx
@@ -9,7 +9,7 @@ export function CountriesList({
   countries,
 }: Readonly<{ countries: Country[] }>) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 p-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-16">
       {countries
         .sort((c1, c2) => c1.name.common.localeCompare(c2.name.common))
         .map((country) => (
@@ -22,7 +22,7 @@ export function CountriesList({
 // --------- COMPONENT SKELETON ---------
 export function CountriesListSkeleton() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 p-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-16">
       {Array.from({ length: 20 }).map((_, i) => (
         <CountryCardSkeleton key={i} />
       ))}
